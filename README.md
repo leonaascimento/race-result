e64d62cbb221704bddcfd35d02c4b7fb1f188bbd
Leonardo Nascimento <leonaascimento@users.noreply.github.com>
2018-11-11 20:31:39 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# race-result
+O race-result é uma ferramenta que a partir do registro das voltas de uma corrida fornece seu resultado
---
+++ src/RaceResult/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RaceResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
